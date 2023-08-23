200f9fb7c2137052610b136d5c67392d6a3f4ce4
jian <0510jian@gmail.com>
2023-08-23 02:18:10 +0000

    ct: gr Day8 update

--- src/Week2/Day8.java
+++ src/Week2/Day8.java
@@ -8,13 +8,10 @@ class Day8 {
 		int N = Integer.parseInt(br.readLine());
 		
 		int countItem = 0;
-		while(N > 0) {
-			if(N >= 14) N -= 14;
-			else if (N >= 7) N -= 7;
-			else N -= 1;
-	
-			countItem++;
-		}
+		countItem += (N / 14);
+		N %= 14;
+		countItem += (N / 7);
+		countItem += N % 7;
 		
 		System.out.println(countItem);
 	}
